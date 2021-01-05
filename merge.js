1b85588765a57fd683d71fb19f36f1cb26b2b524
ermurray <ermurray31@gmail.com>
2021-01-05 23:40:28 +0000

    merge array function completed

---
+++ merge.js
@@ -0,0 +1,7 @@
+function merge(array1, array2){
+  let mergedArray = array1.concat(array2);
+  return mergedArray.sort((a,b) => a - b);
+}
+console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
+console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
+console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
